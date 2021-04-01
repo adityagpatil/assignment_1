e3c4a4e51c5f72712c641e1f895057333a5321c4
adityagpatil <80635666+adityagpatil@users.noreply.github.com>
2021-04-01 18:39:57 +0530

    Add files via upload

---
+++ Memory_39.java
@@ -0,0 +1,37 @@
+/* Implement a Java program that reads via CLA (size type) 
+example: 1 KB / 1 MB, calculate the memory in bytes. */ 
+
+class Memory_39{
+    
+    public static void main(String args[]) {
+        
+		int num = Integer.parseInt(args[0]);
+        if(num < 0)
+        {
+            System.out.println("No negative memory");
+            System.exit(0);
+        }
+        
+        switch(args[1])
+        {
+        case "kb":
+        case "KB":    System.out.println("Memory in bytes: "+(num * Math.pow(2,10)));
+                    break;
+        case "mb":
+        case "MB":    System.out.println("Memory in bytes: "+(num * Math.pow(2,20)));
+                    break;
+        case "gb":
+        case "GB":    System.out.println("Memory in bytes: "+(num * Math.pow(2,30)));
+                    break;
+        case "tb":
+        case "TB":    System.out.println("Memory in bytes: "+(num * Math.pow(2,40)));
+                    break;
+        default:     System.out.println("Wrong input");
+                    break;
+        }
+    
+    }
+
+ 
+
+}
